f24ebc49fa65effa928acd70b59d9fe160dc4005
Eunsoo Roh <roh7@illinois.edu>
2014-02-09 15:47:09 -0600

    Removed SERVER_NAME printout

--- cms/web/app_dev.php
+++ cms/web/app_dev.php
@@ -9,7 +9,6 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-echo $_SERVER['SERVER_NAME'];
 if (strstr($_SERVER['SERVER_NAME'], 'rrec-dev') === FALSE) {
   if (isset($_SERVER['HTTP_CLIENT_IP'])
       || isset($_SERVER['HTTP_X_FORWARDED_FOR'])

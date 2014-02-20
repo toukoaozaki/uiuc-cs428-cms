cafad710dee494672b234fef4c3e37effce279cc
Eunsoo Roh <roh7@illinois.edu>
2014-02-20 14:00:52 -0600

    Fixed formatting problems

--- cms/src/UiucCms/Bundle/UserBundle/Controller/DefaultController.php
+++ cms/src/UiucCms/Bundle/UserBundle/Controller/DefaultController.php
@@ -17,13 +17,16 @@ class DefaultController extends Controller
  
     }
 	
-	public function profileAction()
-	{
-		$user['name'] = 'Eunsoo Roh';
-		$user['company'] = 'CS 429';
-		$user['phone'] = '123456789';
-		$user['email'] = 'roh7@illinois.edu';
-		
-        return $this->render('UiucCmsUserBundle:Default:profile.html.twig', array('user' => $user));
-	}
+    public function profileAction()
+	  {
+        $user['name'] = 'Eunsoo Roh';
+        $user['company'] = 'CS 429';
+        $user['phone'] = '123456789';
+        $user['email'] = 'roh7@illinois.edu';
+
+        return $this->render(
+            'UiucCmsUserBundle:Default:profile.html.twig',
+            array('user' => $user)
+        );
+    }
 }

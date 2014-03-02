5b1430a147c65127bb823fb5bcb8dd02a528f115
dlquach <derekq.alt@gmail.com>
2014-03-02 14:12:27 -0600

    Conference bundle update

--- cms/src/UiucCms/Bundle/ConferenceBundle/Controller/DefaultController.php
+++ cms/src/UiucCms/Bundle/ConferenceBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace UiucCms\Bundle\ConferenceBundle\Controller;
 
+use UiucCms\Bundle\ConferenceBundle\Form\Type\ConferenceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use UiucCms\Bundle\ConferenceBundle\Entity\Conference;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use \DateTime;
@@ -20,8 +23,9 @@ class DefaultController extends Controller
         $conference->setRegisterEndDate(new DateTime("2014-02-31 11:00:15.00"));
         $conference->setTopics([]);
 
+        // These three lines add objects to the database.
         $em = $this->getDoctrine()->getManager();
-        $em->persist($conference);
+        $em->persist($conference); 
         $em->flush();
 
         return new Response('Created product id'.$conference->getId());
@@ -31,7 +35,10 @@ class DefaultController extends Controller
     public function createAction() 
     {
         $conference = new Conference();
-        
+
+        $form = $this->createForm(new ConferenceType(), $conference, array('action' => $this->generateUrl('uiuc_cms_conference_submit'),));
+
+       /* 
         $form = $this->createFormBuilder($conference)
                     ->add('name', 'text')
                     ->add('year', 'integer')
@@ -41,7 +48,21 @@ class DefaultController extends Controller
                     ->add('topics', 'collection', array('type' => 'text'))
                     ->add('create', 'submit')
                     ->getForm();
+                */        
         return $this->render('UiucCmsConferenceBundle:Default:create.html.twig', array( 'form' => $form->createView(),));
+    }
+
+    public function submitAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();             
+        $form = $this->createForm(new ConferenceType(), new Conference());
+        $form->handleRequest($request);
+
+        $conference = $form->getData();
+
+        $em->persist($conference);
+        $em->flush();
+        return new Response('Successfully added element '.$conference->getId().' to database.');
     }
 
 }
--- cms/src/UiucCms/Bundle/ConferenceBundle/Form/Type/ConferenceType.php
+++ cms/src/UiucCms/Bundle/ConferenceBundle/Form/Type/ConferenceType.php
@@ -9,7 +9,18 @@ class ConferenceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        
+        $builder->add('name', 'text');
+        $builder->add('year', 'integer');
+        $builder->add('city', 'text');
+        $builder->add('register_begin_date', 'datetime');
+        $builder->add('register_end_date', 'datetime');
+        $builder->add('topics', 'collection', array('type' => 'text'));
+        $builder->add('create', 'submit');
+    }
+
+    public function getName() 
+    {
+        return 'conference';
     }
 
 }

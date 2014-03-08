cf83c4e532f8dfdb4ea9e6c673ce6e7c5a48bf23
jzhou31 <jason092193@gmail.com>
2014-03-08 15:32:42 -0600

    test

--- cms/src/UiucCms/Bundle/ConferenceBundle/Tests/Controller/DefaultControllerTest.php
+++ cms/src/UiucCms/Bundle/ConferenceBundle/Tests/Controller/DefaultControllerTest.php
@@ -29,35 +29,21 @@ class DefaultControllerTest extends WebTestCase
 	//test that submit successfully added to database
 	public function testSubmit()
     {
-        $conference = new Conference();
-        $conference->setName('Test Conference');
-        $conference->setYear(2014);
-        $conference->setCity('Champaign');
-        $conference->setRegisterBeginDate(new DateTime("2014-02-31 11:00:15.00"));
-        $conference->setRegisterEndDate(new DateTime("2014-02-31 11:00:15.00"));
-        $conference->setTopics([]);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($conference); 
-        $em->flush();
+        $client = static::createClient();
+		$crawler = $client->request('GET', '/conf/create');
+		$form = $crawler->selectButtion('submit')->form(); 
 		
-		$id = $conference->getId();
-		$newConference = $this->getDoctrine()
-                            ->getRepository('UiucCmsConferenceBundle:Conference')
-                            ->find($id);
-        if (!$newConference) {
-            $this->assertEquals(2,3);
-        }
-
-        else {
-            $this->assertEquals('Test Conference', $newConference->getName());
-			$this->assertEquals(2014, $newConference->getYear());
-			$this->assertEquals('Champaign', $newConference->getCity());
-			
-        }
-
-		$em->remove($newConference);
-		$em->flush();
+		$form['name'] = 'Test';
+		$form['year'] = 2014;
+		$form['registerBeginDate'] = new DateTime("2014-02-31 11:00:15.00");
+		$form['registerEndDate'] = new DateTime("2014-02-31 11:00:15.00");
+		$form['topics'] = [];
+		
+		$crawler = $client->submit($form);
+		
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+		$this->assertTrue($crawler->filter('html:contains("Created product id")')->count() > 0);
+		
         
     }
 	

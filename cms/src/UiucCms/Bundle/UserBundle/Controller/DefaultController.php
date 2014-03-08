f38de4f24614a1ffd430ab40b561d0e1e012fb4d
Eunsoo Roh <roh7@illinois.edu>
2014-03-08 14:53:00 -0600

    Layout and view fixes

--- cms/src/UiucCms/Bundle/UserBundle/Controller/DefaultController.php
+++ cms/src/UiucCms/Bundle/UserBundle/Controller/DefaultController.php
@@ -8,19 +8,9 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('UiucCmsUserBundle:Default:index.html.twig');
-    }
-
-    public function profileAction()
-	  {
-        $user['name'] = 'Eunsoo Roh';
-        $user['company'] = 'CS 429';
-        $user['phone'] = '123456789';
-        $user['email'] = 'roh7@illinois.edu';
-
         return $this->render(
-            'UiucCmsUserBundle:Default:profile.html.twig',
-            array('user' => $user)
+            'UiucCmsUserBundle:Default:index.html.twig',
+            array('user' => $this->getUser())
         );
     }
 }

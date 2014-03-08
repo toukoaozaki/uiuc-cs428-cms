611ad5370d2e80c858c300690b945dd3ae30cd00
jzhou31 <jason092193@gmail.com>
2014-03-08 14:34:27 -0600

    added test for conference bundle

--- cms/src/UiucCms/Bundle/ConferenceBundle/Tests/Controller/DefaultControllerTest.php
+++ cms/src/UiucCms/Bundle/ConferenceBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,62 @@
 namespace UiucCms\Bundle\ConferenceBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use UiucCms\Bundle\ConferenceBundle\Form\Type\ConferenceType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use UiucCms\Bundle\ConferenceBundle\Entity\Conference;
+
+use \DateTime;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+		$this->assertEquals(3,3);
     }
+	
+	//test that create page exists
+	public function testCreate()
+	{
+		$client = static::createClient();
+
+        $crawler = $client->request('GET', '/conf/create');
+
+        $this->assertTrue($crawler->filter('html:contains("Create a new conference")')->count() > 0);
+	}
+	
+	//test that submit successfully added to database
+	public function testSubmit()
+    {
+        $conference = new Conference();
+        $conference->setName('Test Conference');
+        $conference->setYear(2014);
+        $conference->setCity('Champaign');
+        $conference->setRegisterBeginDate(new DateTime("2014-02-31 11:00:15.00"));
+        $conference->setRegisterEndDate(new DateTime("2014-02-31 11:00:15.00"));
+        $conference->setTopics([]);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($conference); 
+        $em->flush();
+		
+		$id = $conference->getId();
+		$newConference = $this->getDoctrine()
+                            ->getRepository('UiucCmsConferenceBundle:Conference')
+                            ->find($id);
+        if (!$newConference) {
+            $this->assertEquals(2,3);
+        }
+
+        else {
+            $this->assertEquals('Test Conference', $newConference->getName());
+			$this->assertEquals(2014, $newConference->getYear());
+			$this->assertEquals('Champaign', $newConference->getCity());
+			
+        }
+
+		$em->remove($newConference);
+		$em->flush();
+        
+    }
+	
 }

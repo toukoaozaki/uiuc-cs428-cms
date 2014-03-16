1d293c90c73c1c245834fd9580c9362ea2cc1a16
jzhou31 <jason092193@gmail.com>
2014-03-16 15:28:46 -0500

    added tests to promote user logic

--- cms/src/UiucCms/Bundle/AdminBundle/Tests/Controller/DefaultControllerTest.php
+++ cms/src/UiucCms/Bundle/AdminBundle/Tests/Controller/DefaultControllerTest.php
@@ -14,4 +14,29 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
+		
+	/*
+		test that super admin will not be displayed in the list of users that can be promoted
+	*/
+	public function testShow()
+	{
+		$client = static::createClient();
+
+        $crawler = $client->request('GET', '/user/admin/show');
+
+        $this->assertTrue($crawler->filter('html:contains("admin@domain.com")')->count() == 0);	
+	}
+	
+	/* 
+		test that admin will not be displayed 
+	*/
+	
+	public function testShow()
+	{
+		$client = static::createClient();
+
+        $crawler = $client->request('GET', '/user/admin/show');
+
+        $this->assertTrue($crawler->filter('html:contains("test@uiuc.edu")')->count() == 0);	
+	}
 }

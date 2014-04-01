122ec95a1d79faf0681dc092f3d593b92b02d30c
Eunsoo Roh <roh7@illinois.edu>
2014-04-01 18:23:03 -0500

    Implemented more payment controller tests

--- cms/src/UiucCms/Bundle/PaymentBundle/Tests/Controller/PaymentControllerTest.php
+++ cms/src/UiucCms/Bundle/PaymentBundle/Tests/Controller/PaymentControllerTest.php
@@ -20,8 +20,33 @@ class PaymentControllerTest extends WebTestCase
 
     public function testChoosePaymentMethodGet()
     {
-        $this->client->request(
+        $crawler = $this->client->request(
             'GET',
+            $this->router->generate(
+                'uiuc_cms_payment_start',
+                array('order' => LoadTestOrder::TEST_ORDER_NUMBER)
+            )
+        );
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        // must show payment amount and currency
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_AMOUNT.'")'
+            )->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_CURRENCY.'")'
+            )->count()
+        );
+    }
+
+    public function testChoosePaymentMethodPost()
+    {
+        $this->client->request(
+            'POST',
             $this->router->generate(
                 'uiuc_cms_payment_start',
                 array('order' => LoadTestOrder::TEST_ORDER_NUMBER)
@@ -53,10 +78,88 @@ class PaymentControllerTest extends WebTestCase
 
     public function testPaymentSuccess()
     {
+        $crawler = $this->client->request(
+            'GET',
+            $this->router->generate(
+                'uiuc_cms_payment_start',
+                array('order' => LoadTestOrder::TEST_ORDER_NUMBER)
+            )
+        );
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        // use "Success" method
+        $form = $crawler->selectButton('Continue')->form();
+        $form['jms_choose_payment_method[method]']->select('dummy_success');
+        $this->client->submit($form);
+        // must be a redirection
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $crawler = $this->client->followRedirect();
+        // the result page should be a success
+        $this->assertEquals(
+            0,
+            $crawler->filter('html:contains("failed")')->count()
+        );
+        // the result page must have order number, amount, currency
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_NUMBER.'")'
+            )->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_AMOUNT.'")'
+            )->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_CURRENCY.'")'
+            )->count()
+        );
     }
 
     public function testPaymentFailure()
     {
+        $crawler = $this->client->request(
+            'GET',
+            $this->router->generate(
+                'uiuc_cms_payment_start',
+                array('order' => LoadTestOrder::TEST_ORDER_NUMBER)
+            )
+        );
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        // use "Success" method
+        $form = $crawler->selectButton('Continue')->form();
+        $form['jms_choose_payment_method[method]']->select('dummy_failure');
+        $this->client->submit($form);
+        // must be a redirection
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $crawler = $this->client->followRedirect();
+        // the result page should be a failure
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("failed")')->count()
+        );
+        // the result page must have order number, amount, currency
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_NUMBER.'")'
+            )->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_AMOUNT.'")'
+            )->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter(
+                'html:contains("'.LoadTestOrder::TEST_ORDER_CURRENCY.'")'
+            )->count()
+        );
     }
 
     private function setupFixtures($container)

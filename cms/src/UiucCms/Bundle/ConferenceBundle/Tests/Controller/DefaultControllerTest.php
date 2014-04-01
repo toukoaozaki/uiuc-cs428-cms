7c913f1ffc56a55cca841460f2929029407f0a86
dlquach <derekq.alt@gmail.com>
2014-04-01 18:41:40 -0500

    Added index test

--- cms/src/UiucCms/Bundle/ConferenceBundle/Tests/Controller/DefaultControllerTest.php
+++ cms/src/UiucCms/Bundle/ConferenceBundle/Tests/Controller/DefaultControllerTest.php
@@ -7,13 +7,56 @@ use UiucCms\Bundle\ConferenceBundle\Form\Type\ConferenceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use UiucCms\Bundle\ConferenceBundle\Entity\Conference;
 
+use UiucCms\Bundle\UserBundle\DataFixtures\ORM\Test\LoadTestUser;
+use UiucCms\Bundle\UserBundle\DataFixtures\ORM\Common\LoadSuperuser;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+
+
 use \DateTime;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private $client;
+    private $router;
+    private $login_url;
+    private $profile_url;
+
+    private function setupFixtures($container)
+    {
+        // get entity manager
+        $em = $container->get('doctrine')->getManager();
+        $purger = new ORMPurger($em);
+        $executor = new ORMExecutor($em, $purger);
+        // purge fixtures
+        $executor->purge();
+        // load fixtures
+        $loader = new Loader();
+        $fixtures = new LoadTestUser();
+        $fixtures->setContainer($container);
+        $loader->addFixture($fixtures);
+        $executor->execute($loader->getFixtures());
+    }
+
+    protected function setUp()
+    {
+        $this->client = static::createClient();
+        $this->container = $this->client->getContainer();
+        $this->setupFixtures($this->container);
+        $this->router = $this->container->get('router');
+        $this->index_url = $this->router->generate(
+            'uiuc_cms_conference_homepage',
+            array(),
+            true);
+    }
+
     public function testIndex()
     {
-        $this->assertEquals(3,3);
+        $crawler = $this->client->request('GET', $this->index_url);
+        $this->assertEquals(
+            0,
+            $crawler->filter('html:contains("No conferences found.")')->count());
     }
 
     //test that create page exists
@@ -49,5 +92,7 @@ class DefaultControllerTest extends WebTestCase
       $this->assertTrue($client->getResponse()->isSuccessful());
       $this->assertTrue($crawler->filter('html:contains("Successfully added element")')->count() > 0);
   } */
+
+  
 
 }

d807264273c65a969c18f919fbfbafa6879333f9
kmangutov <kmangutov@gmail.com>
2014-04-06 16:33:54 -0500

    add mail entity

---
+++ cms/src/UiucCms/Bundle/AdminBundle/Entity/Mail.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace UiucCms\Bundle\AdminBundle\Entity;
+
+
+
+/**
+ * Mail entity for mass mailing
+ */
+class Mail
+{
+    private $to;
+    private $from;
+    private $subject;
+    private $body;
+    
+    public function Mail() 
+    {
+        $this->to = array();
+    }
+    
+    public function setTo($addresses)
+    {
+        $this->to = $addresses;
+    }
+    
+    public function addTo($address)
+    {
+        $this->to[] = $address;
+    }
+    
+    public function setFrom($address)
+    {
+        $this->from = $address;
+    }
+    
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+    
+    public function setSubject($text)
+    {
+        $this->subject = $text;
+    }
+    
+    public function getBody()
+    {
+        return $this->body;
+    }
+    
+    public function setBody($text)
+    {
+        $this->body = $text;
+    }
+    
+    public function sendMail()
+    {
+        $message = Swift_Message::newInstance()
+            ->setSubject($this->subject)
+            ->setFrom($this->from)
+            ->setBody(
+                $this->renderView(
+                    'UiucCmsAdminBundle:Default:mail.txt.twig',
+                    array('body' => $body)
+                )
+            )
+        ;
+        
+        //get Swift_Message bundle working to use
+        //$this->get('mailer')->send($message);
+    }   
+}
+
+?>
